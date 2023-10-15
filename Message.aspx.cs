4cfcb7aee16c0ff63cab45f1c32ac03b5ee962b6
vamsi Reddy <vamsigone001@gmail.com>
2023-10-15 19:42:44 +0530

    added message.aspx

---
+++ Message.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SignalRChat
+{
+    public partial class Message : System.Web.UI.Page
+    {
+        public string UserName = "admin";
+        ConnClass ConnC = new ConnClass();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["UserName"] != null)
+            {
+                UserName = Session["UserName"].ToString();
+               // GetUserImage(UserName);
+            }
+            else
+                Response.Redirect("Login.aspx");
+
+            //this.Header.DataBind();
+        }
+    }
+}
